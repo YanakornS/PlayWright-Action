2c3bd8a97cfd929e1b32ca6792d2849f1838f01a
YanakornS <654259002@webmail.npru.ac.th>
2025-09-02 10:20:07 +0700

    Merg

--- tests/test-project.spec.ts
+++ tests/test-project.spec.ts
@@ -1,21 +1,16 @@
 // import { test, expect } from '@playwright/test';
 
-// test('test', async ({ page }) => {
-//   await page.goto('https://dormdeal-project.vercel.app/');
-//   await page.locator('[data-test="login-sign"]').click();
-//   await page.locator('[data-test="LoginNpru-button"]').click();
-//   await page.locator('[data-test="scroll-bottom"]').click();
-//   const page1Promise = page.waitForEvent('popup');
-//   await page.locator('[data-test="accept-button"]').click();
-//   const page1 = await page1Promise;
-//   await page1.getByRole('textbox', { name: 'Enter your email' }).click();
-//   await page1.getByRole('textbox', { name: 'Enter your email' }).fill('654259002');
-//   await page1.getByRole('button', { name: 'Next' }).click();
-//   await page1.getByRole('textbox', { name: 'Enter your password' }).fill('1730201408866');
-//   await page1.getByRole('textbox', { name: 'Enter your password' }).click();
-// });
-
-
-//
-//
-//X
+test('test', async ({ page }) => {
+  await page.goto('https://dormdeal-project.vercel.app/');
+  await page.locator('[data-test="login-sign"]').click();
+  await page.locator('[data-test="LoginNpru-button"]').click();
+  await page.locator('[data-test="scroll-bottom"]').click();
+  const page1Promise = page.waitForEvent('popup');
+  await page.locator('[data-test="accept-button"]').click();
+  const page1 = await page1Promise;
+  await page1.getByRole('textbox', { name: 'Enter your email' }).click();
+  await page1.getByRole('textbox', { name: 'Enter your email' }).fill('654259002');
+  await page1.getByRole('button', { name: 'Next' }).click();
+  await page1.getByRole('textbox', { name: 'Enter your password' }).fill('1730201408866');
+  await page1.getByRole('textbox', { name: 'Enter your password' }).click();
+});
